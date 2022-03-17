2b787ecda41270c833af569cf2ea95de5adfc8c9
davidprager <david_prager@yahoo.com>
2022-03-16 20:02:06 -0400

    Initial commit

---
+++ strings.js
@@ -0,0 +1,44 @@
+/* This file contains several functions that manipulate strings */
+
+
+"use strict"
+const STRING = "To be or not to be, that is the question!"
+const SEARCH = "u";
+const REPLACE = "x";
+const NUMBER = 2;
+
+// This program manipulates an input string and displays some statistics about it
+function manipulateString() {
+
+}
+
+/* This function removes punctuation from a string and returns the updated string*/
+function cleanString(str){
+
+}
+
+/* This function will replace a specified character with an alternative and return the updated string */
+function replaceCharacters(str, search, replace) {
+
+}
+/* This function will return the number of vowels in a string */
+function vowelCount(str) {
+
+}
+
+// This function will return the character in a string that has the lowest ASCII value
+function getMin(str) {
+
+}
+
+// This function will return the character in a string that has the highest ASCII value
+function getMax(str) {
+
+}
+
+// This function will return the number of words in a string with a given length
+function countWords(str, number) {
+
+}
+
+
---
+++ username.js
@@ -0,0 +1,9 @@
+/* This file contains functions to validate a username
+*/
+
+"use strict"
+
+/* This function will validate a username */
+function validateUsername(username) {
+
+}
